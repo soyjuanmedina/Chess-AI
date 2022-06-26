ee727374fc90c64f88c0a98cd6b506cc6ff66486
Juan Medina <soyjuanmedina@gmail.com>
2022-06-26 16:34:54 +0200

    Two tboard types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,70 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit, AfterViewInit {
   title = 'Ajedrez';
-  squareSize = 75
-  lightSquare = '#842A0D';
-  darkSquare = '#F1CDC1';
+  startFEN: string = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+  squareSize: number = 75;
+  lightSquare: string = '#842A0D';
+  darkSquare: string = '#F1CDC1';
   canvas;
   ctx;
+  isDragging=false;
 
 
   ngOnInit() {
-    this.canvas = <HTMLCanvasElement>document.getElementById('board');
-    this.ctx = this.canvas.getContext('2d');
-    this.ctx.beginPath();
-    for (let file = 0; file < 8; file++) {
-      for (let rank = 0; rank < 8; rank++) {
-        this.drawSquare(this.getSquareColor(file, rank), file * this.squareSize, rank * this.squareSize);
+    // this.canvas = <HTMLCanvasElement>document.getElementById('board');
+    // this.ctx = this.canvas.getContext('2d');
+    // this.ctx.beginPath();
+    // for (let file = 0; file < 8; file++) {
+    //   for (let rank = 0; rank < 8; rank++) {
+    //     this.drawSquare(this.getSquareColor(file, rank), file * this.squareSize, rank * this.squareSize);
+    //   }
+    // }
+    // this.loadPositionFromFem(this.startFEN);
+  }
+
+  loadPositionFromFem(fen: string) {
+    let fenBoard: string = fen.split(' ')[0];
+    let x: number = 0;
+    let y: number = 0;
+    [...fenBoard].forEach(symbol => {
+      if(symbol == '/'){ 
+        x = 0;
+        y ++;
+      } else{
+        if(symbol >= '0' && symbol <= '9'){
+          x = x + parseInt(symbol);
+        } else{
+          let color: string = 'w';
+          if (symbol == symbol.toLowerCase()) {
+            color = 'b';
+          }
+          let piece = symbol.toLowerCase();
+          // this.drawPiece(color, symbol.toLowerCase(), x * this.squareSize, y * this.squareSize);
+          this.drawHTMLPiece(color, piece, x, y);
+          x ++;
+        }
       }
+
+    });
+  }
+
+  drawPiece(color: string, piece: string, x: number, y: number){
+    let piece_image = new Image();
+    piece_image.src = 'assets/pieces/' + color + piece + '.png';
+    piece_image.onload = () => {
+      this.ctx.drawImage(piece_image, x, y, this.squareSize, this.squareSize);
     }
   }
 
   drawSquare(color, file, rank) {
-    console.log(color, file, rank);
     this.ctx.beginPath();
     this.ctx.rect(file, rank, this.squareSize, this.squareSize);
     this.ctx.fillStyle = color;
     this.ctx.fill();
     this.ctx.closePath();
   }
+
+
 
   getSquareColor(file, rank) {
     if ((file + rank) % 2 == 0) {
@@ -43,23 +81,28 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   }
 
-  ngAfterViewInit(): void {
+  drawHTMLBoard(){
     var section = document.querySelector('#board');
-    for (var i = 0; i <= 7; i++) {
-      var fila = document.createElement("div"); // Para aplicar saltos de línea separamos las filas
-      section.appendChild(fila);
-      for (var j = 0; j <= 7; j++) { // La misma iteración pero pintando los cuadrados indivualmente
+    for (var x = 0; x <= 7; x++) {
+      var row = document.createElement("div"); // Para aplicar saltos de línea separamos las filas
+      section.appendChild(row);
+      for (var y = 0; y <= 7; y++) { // La misma iteración pero pintando los cuadrados indivualmente
         var element = document.createElement("div");
-        fila.appendChild(element);
-        if (i % 2 == 0) {
-          if (j % 2 == 0) {
+        row.appendChild(element);
+        let position = String.fromCharCode(97 + x) + (y+1);
+        element.setAttribute("id", position);
+        // element.addEventListener("dragenter", this.dragenter);
+        // element.addEventListener("dragleave", this.dragleave);
+        element.addEventListener("dragover", this.allowDrop);
+        element.addEventListener("drop", this.drop.bind(this));
+        if (x % 2 == 0) {
+          if (y % 2 == 0) {
             element.classList.add('dark');
-            // element.classList.add('addBorder');
           } else {
             element.classList.add('ligth');
           }
         } else {
-          if (j % 2 != 0) {
+          if (y % 2 != 0) {
             element.classList.add('dark');
           } else {
             element.classList.add('ligth');
@@ -68,25 +111,39 @@ export class AppComponent implements OnInit, AfterViewInit {
 
       }
     }
+    
+  }
 
-    // El bucle iterará 8 veces (de 0 a 7), que son las filas que tiene un tablero de ajedrez, 
-    // y por cada iteración en una fila se efectuarán las iteración por columnas que este tiene (8)
-    /* for (var i = 0; i <= 7; i++) {  // Iteración por "fila"
+  drawHTMLPiece(color, piece, x, y){
+    let position = String.fromCharCode(97 + x) + (y+1);
+    let innerDiv = document.getElementById(position);
+    let img: HTMLImageElement = document.createElement("img");
+    img.src = 'assets/pieces/' + color + piece + '.png';
+    img.style.width = "50px";
+    img.style.height = "50px";
+    img.setAttribute("id", color + piece + position);
+    img.setAttribute("draggable", "true");
+    img.addEventListener('dragstart', this.drag);
+    innerDiv.appendChild(img);
+  }
 
-      var fila = document.createElement("div"); // Para aplicar saltos de línea separamos las filas
-      section.appendChild(fila);
+  ngAfterViewInit(): void {
+    this.drawHTMLBoard();
+    this.loadPositionFromFem(this.startFEN);
+  }
+  
+  allowDrop(ev) {
+    ev.preventDefault();
+  }
 
-      for (var j = 0; j <= 7; j++) { // La misma iteración pero pintando los cuadrados indivualmente
+  drag(ev) {
+    ev.dataTransfer.setData("piece", ev.target.id);
+  }
 
-        var element = document.createElement("div");
-        fila.appendChild(element);
-
-        if (i % 2 == 0 && j % 2 != 0 || i % 2 != 0 && j % 2 == 0) { // Aquí vemos si la relación fila-columna es par o impar, de esta manera implementaremos de manera alternada una clase para aplicarle por css un background distinto y hacer el efecto de tablero de ajedrez
-          element.classList.add('oscuro');
-        }
-      }
-      section.appendChild(fila);
-    } */
+  drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("piece");
+    ev.target.appendChild(document.getElementById(data));
   }
 
 }
