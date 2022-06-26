3b0f09ff939fc8ed4185a640d83dc7633f504c38
Juan Medina <soyjuanmedina@gmail.com>
2022-06-26 18:34:26 +0200

    Set scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,29 +3,19 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+
+export class AppComponent implements OnInit {
   title = 'Ajedrez';
   startFEN: string = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
   squareSize: number = 75;
   lightSquare: string = '#842A0D';
   darkSquare: string = '#F1CDC1';
-  canvas;
-  ctx;
-  isDragging=false;
+  isConfiguring: boolean = false;
 
-
-  ngOnInit() {
-    // this.canvas = <HTMLCanvasElement>document.getElementById('board');
-    // this.ctx = this.canvas.getContext('2d');
-    // this.ctx.beginPath();
-    // for (let file = 0; file < 8; file++) {
-    //   for (let rank = 0; rank < 8; rank++) {
-    //     this.drawSquare(this.getSquareColor(file, rank), file * this.squareSize, rank * this.squareSize);
-    //   }
-    // }
-    // this.loadPositionFromFem(this.startFEN);
+  setConfiguring(){
+    this.isConfiguring = !this.isConfiguring;
   }
 
   loadPositionFromFem(fen: string) {
@@ -45,54 +35,23 @@ export class AppComponent implements OnInit, AfterViewInit {
             color = 'b';
           }
           let piece = symbol.toLowerCase();
-          // this.drawPiece(color, symbol.toLowerCase(), x * this.squareSize, y * this.squareSize);
-          this.drawHTMLPiece(color, piece, x, y);
+          this.drawPiece(color, piece, x, y);
           x ++;
         }
       }
-
     });
   }
 
-  drawPiece(color: string, piece: string, x: number, y: number){
-    let piece_image = new Image();
-    piece_image.src = 'assets/pieces/' + color + piece + '.png';
-    piece_image.onload = () => {
-      this.ctx.drawImage(piece_image, x, y, this.squareSize, this.squareSize);
-    }
-  }
-
-  drawSquare(color, file, rank) {
-    this.ctx.beginPath();
-    this.ctx.rect(file, rank, this.squareSize, this.squareSize);
-    this.ctx.fillStyle = color;
-    this.ctx.fill();
-    this.ctx.closePath();
-  }
-
-
-
-  getSquareColor(file, rank) {
-    if ((file + rank) % 2 == 0) {
-      return this.lightSquare;
-    } else {
-      return this.darkSquare;
-    }
-
-  }
-
-  drawHTMLBoard(){
+  drawBoard(){
     var section = document.querySelector('#board');
     for (var x = 0; x <= 7; x++) {
-      var row = document.createElement("div"); // Para aplicar saltos de línea separamos las filas
+      var row = document.createElement("div"); 
       section.appendChild(row);
-      for (var y = 0; y <= 7; y++) { // La misma iteración pero pintando los cuadrados indivualmente
+      for (var y = 0; y <= 7; y++) { 
         var element = document.createElement("div");
         row.appendChild(element);
         let position = String.fromCharCode(97 + x) + (y+1);
         element.setAttribute("id", position);
-        // element.addEventListener("dragenter", this.dragenter);
-        // element.addEventListener("dragleave", this.dragleave);
         element.addEventListener("dragover", this.allowDrop);
         element.addEventListener("drop", this.drop.bind(this));
         if (x % 2 == 0) {
@@ -108,13 +67,12 @@ export class AppComponent implements OnInit, AfterViewInit {
             element.classList.add('ligth');
           }
         }
-
       }
     }
     
   }
 
-  drawHTMLPiece(color, piece, x, y){
+  drawPiece(color, piece, x, y){
     let position = String.fromCharCode(97 + x) + (y+1);
     let innerDiv = document.getElementById(position);
     let img: HTMLImageElement = document.createElement("img");
@@ -125,11 +83,6 @@ export class AppComponent implements OnInit, AfterViewInit {
     img.setAttribute("draggable", "true");
     img.addEventListener('dragstart', this.drag);
     innerDiv.appendChild(img);
-  }
-
-  ngAfterViewInit(): void {
-    this.drawHTMLBoard();
-    this.loadPositionFromFem(this.startFEN);
   }
   
   allowDrop(ev) {
@@ -144,6 +97,11 @@ export class AppComponent implements OnInit, AfterViewInit {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("piece");
     ev.target.appendChild(document.getElementById(data));
+  }
+
+  ngOnInit() {
+    this.drawBoard();
+    this.loadPositionFromFem(this.startFEN);
   }
 
 }
